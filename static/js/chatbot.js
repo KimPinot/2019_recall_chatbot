44f2863319b36d93ca408cd5ac6866a68b9509b6
KimPinot <sohuyn43@gmail.com>
2019-11-27 23:53:33 +0900

    🔨 botchat 구현 거의 완료

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', () => {
     const chatBotValue = document.getElementById("JSchatBotValue");
     const chatBotSubmit = document.getElementById("JSchatBotSubmit");
 
@@ -17,5 +17,43 @@ window.addEventListener('DOMContentLoaded', function(){
     const chatBotSend = () => {
         console.log(chatBotValue.value);
         chatBotValue.value = "";
-    }
+    };
+
+    // var urls = '';
+    // var opts = {
+    //     method: 'POST',
+    //     body: '{longUrl:"' + urls + '"}',
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // };
+    // fetch('', opts).then(function (response) {
+    //     return response.json();
+    // }).then(function (body) {
+    //     window.Unity.call(JSON.parse(JSON.stringify(body)).id);
+    // });
+
+        let data = `
+        <div class="botchat" data-chatidx="` + 1 +`">
+        <div class="profile">
+          <img src="//placehold.it/50x50">
+        </div>
+
+        <div class="chat">
+          <span>` + '테스트' + `</span>
+          <div class="function">
+            <div class="fake"></div>
+            <ul class="chatBotSetis">
+              <li id="JSchatBotSetisYes">
+                <i class="far fa-thumbs-up"></i>
+              </li>
+              <li id="JSchatBotSetisNo">
+                <i class="far fa-thumbs-down"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    `;
 });
