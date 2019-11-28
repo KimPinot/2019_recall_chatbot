27b1ad7738ca39ce1500b91a35291a6754413cb1
KimPinot <sohuyn43@gmail.com>
2019-11-28 13:50:46 +0900

    🔨 에러 기능, 웰컴 메시지 같은 소소한 기능 추가

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -4,27 +4,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const chatBotBody = document.getElementById("chatBody");
 
-    // AJAX
-    const AJAX = (method, link, data) => {
-        const xhttp = new XMLHttpRequest();
-
-        // 요청 성공시
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState === 4 && xhttp.status === 200) {
-                r = xhttp.responseText;
-            } else {
-                // 요청 실패시 경고를 띄움
-                console.warn(xhttp.responseText);
-            }
-        };
-        xhttp.open(method, link, true);
-        xhttp.setRequestHeader('Content-type', 'application/json');
-        xhttp.send(data);
-
-        console.log([r]);
-        return [r];
-    };
-
     // 챗봇으로 텍스트를 작성하는 코드
     chatBotValue.addEventListener('keyup', () => {
         if (window.event.keyCode === 13) {
@@ -37,8 +16,13 @@ window.addEventListener('DOMContentLoaded', () => {
         chatBotSend();
     });
 
-    // 사용자가 메시지를 보내는 기능 추가
+    // 사용자가 메시지를 보내는 기능
     const chatBotSend = () => {
+        if (chatBotValue.value === "") {
+            alert('내용을 입력해주세요!');
+            return false;
+        }
+
         const data = `
             <div class="userchat">
                 <span class="chat">
@@ -50,10 +34,24 @@ window.addEventListener('DOMContentLoaded', () => {
         chatBotValue.value = "";
     };
 
-    const botChat = (setisbool) => {
+    // 챗봇이 메시지를 보내는 기능
+    const botChat = (setisbool, sendtext) => {
         const chatbotidx = 1;
-        const chatbottext = "테스트";
+        let chatbottext = "";
         let setisform = "";
+
+        // 웰컴 메세지 출력
+        if (sendtext === "welcome") {
+            chatbottext = `
+                안녕하세요. 리콜봇입니다!
+                <br>
+                궁금하신 사항이 있으시다면, 무엇이든지 물어보세요!
+                <br>
+                응답에 만족도를 입력해주시면, 챗봇이 더 똑똑해질거에요!
+            `;
+        } else {
+            chatbottext = sendtext;
+        }
 
         // 값이 true 일 경우, 만족도 조사 기능 추가
         if (setisbool === true) {
@@ -88,6 +86,49 @@ window.addEventListener('DOMContentLoaded', () => {
         chatBotBody.innerHTML += data;
     };
 
+    // AJAX
+    const AJAX = (method, link, data) => {
+        const xhttp = new XMLHttpRequest();
 
-    botChat();
+        if (method === link || null) {
+            error(1);
+            return false;
+        }
+
+        // 요청 성공시
+        xhttp.onreadystatechange = function () {
+            console.log(xhttp.readyState);
+
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                r = xhttp.responseText;
+            } else {
+                // 요청 실패시 경고를 띄움
+                error();
+            }
+        };
+        xhttp.open(method, link, true);
+        xhttp.setRequestHeader('Content-type', 'application/json');
+        xhttp.send(data);
+
+        console.log([r]);
+        return [r];
+    };
+
+    // 에러 발생시 오류를 작성하는 코드
+    const error = (errorCode) => {
+        let query, errorText;
+
+        if (errorCode === 1) {
+            query = "[AJAX] 오류가 발생했습니다. 오류코드 001";
+            errorText = "[AJAX] 필수 인수가 누락되었습니다.";
+        } else {
+            query = "알 수 없는 오류가 발생했습니다. 오류코드 000";
+            errorText = "[JavaScript] 콘솔의 오류 내용을 확인해주세요."
+        }
+
+        botChat(false, query);
+        console.error(errorText);
+    };
+
+    botChat(false, "welcome")
 });
