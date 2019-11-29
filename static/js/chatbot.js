4c4b0948cd6aa970ee03d84a472539cbe53f986c
KimPinot <sohuyn43@gmail.com>
2019-11-30 07:29:46 +0900

    🔨 아아니 이게 왜 되는거지

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -1,6 +1,9 @@
 let answerarray = "";
 let lastQuestion = "";
+let recallarray = "";
+let recallsubarray = "";
 let answerSelectLength = -1;
+let recallSelectLength = -1;
 let query = "";
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,72 +11,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const chatBotSubmit = document.getElementById("JSchatBotSubmit");
     const chatBotBody = document.getElementById("chatBody");
     const answerSelect = document.getElementsByClassName("JSchatBotAnswerSelect");
+    const recallSelect = document.getElementsByClassName("JSrecallSelect");
     const applyForm = document.getElementById('apply');
     const user_id = "0xe292c994516c8b35c9743b260ec2086d1a47e14d";
-
-    // 채팅 입력시 하단으로 강제 스크롤
-    const bottomScroll = () => {
-        chatBotBody.scrollTo(0, chatBotBody.scrollHeight);
-    };
-
-    // 버튼에 자동으로 이벤트 리스너 추가
-    const addEvent = () => {
-
-        if (answerSelect.length === answerSelectLength) {
-            return false;
-        }
-
-        for (let i = 0; i < answerSelect.length; i++) {
-            let j = i;
-            if (i > answerSelectLength) {
-                const t = Number(answerSelect[i].getAttribute("data-idx")) + 1;
-                j = t + answerSelectLength;
-            }
-            const c = answerSelect[i].getAttribute("data-idx");
-
-            const dooooo = () => {
-                if (answerSelect[j].innerHTML === lastQuestion) {
-                    alert('[선택해서보내기] 똑같은 질의를 여러번 할 수 없습니다!');
-                    return false;
-                }
-                const v =
-                    answerSelect[c].getAttribute('data-answer') + `
-                    <br>
-                    <a href="http://` + answerarray[c].Link + `" target="_blank">자세히보기</a>
-                    <br>
-                    <button id="JSchatBotRecallForm">
-                        리콜 신청하기
-                    </button>
-                `;
-
-                chatSend(answerSelect[j].innerHTML, false, "query");
-                botChat(v);
-
-                const f = document.getElementById('JSchatBotRecallForm');
-                f.addEventListener('click', () => {
-                    chatSend("신청" + answerSelect[j].innerHTML, false, "apply");
-                    apply(answerarray[c].Link_Gubun, answerarray[c].Question);
-                });
-                f.removeAttribute("id");
-            };
-
-            // console.log([j, c]);\
-            // console.log(answerSelect[j].getAttribute('eventseted'));
-            if (answerSelect[j].getAttribute('eventseted') !== "true") {
-                // console.log('이벤트가 감지되지 않음');
-                answerSelect[j].addEventListener("click", () => {
-                    dooooo();
-                    answerSelect[j].setAttribute('eventseted', "true");
-                });
-            }
-        }
-
-        if (answerSelect.length !== 0) {
-            answerSelectLength = answerSelect.length - 1;
-        }
-        // console.log([Number(answerSelectLength + 1), answerSelect.length]);
-        // console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-    };
 
     // 챗봇으로 텍스트를 작성하는 코드
     chatBotValue.addEventListener('keyup', () => {
@@ -83,15 +23,139 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // 보내기 버튼이 눌렸을때 텍스트를 보내는 코드
+// 보내기 버튼이 눌렸을때 텍스트를 보내는 코드
     chatBotSubmit.addEventListener('click', () => {
         chatSend(chatBotValue.value, true);
         bottomScroll();
     });
 
+    // 채팅 입력시 하단으로 강제 스크롤
+    const bottomScroll = () => {
+        chatBotBody.scrollTo(0, chatBotBody.scrollHeight);
+    };
+
+    // 버튼에 자동으로 이벤트 리스너 추가
+    const addEvent = () => {
+        if (answerSelect.length > 0) {
+            if (answerSelect.length === answerSelectLength) {
+                return false;
+            }
+
+            for (let i = 0; i < answerSelect.length; i++) {
+                let j = i;
+                if (i > answerSelectLength) {
+                    const t = Number(answerSelect[i].getAttribute("data-idx")) + 1;
+                    j = t + answerSelectLength;
+                }
+                const c = answerSelect[i].getAttribute("data-idx");
+
+                const dooooo = () => {
+                    if (answerSelect[j].innerHTML === lastQuestion) {
+                        alert('[선택해서보내기] 똑같은 질의를 여러번 할 수 없습니다!');
+                        return false;
+                    }
+                    const v =
+                        answerSelect[c].getAttribute('data-answer') + `
+                    <br>
+                    <a href="http://` + answerarray[c].Link + `" target="_blank">자세히보기</a>
+                    <br>
+                    <button id="JSchatBotRecallForm">
+                        리콜 신청하기
+                    </button>
+                `;
+
+                    chatSend(answerSelect[j].innerHTML, false);
+                    botChat(v);
+
+                    const f = document.getElementById('JSchatBotRecallForm');
+                    f.addEventListener('click', () => {
+                        chatSend("신청" + answerSelect[j].innerHTML, false);
+                        apply(answerarray[c].Link_Gubun, answerarray[c].Question);
+                    });
+                    f.removeAttribute("id");
+                };
+
+                // console.log([j, c]);\
+                // console.log(answerSelect[j].getAttribute('eventseted'));
+                if (answerSelect[j].getAttribute('eventseted') !== "true") {
+                    // console.log('이벤트가 감지되지 않음');
+                    answerSelect[j].addEventListener("click", () => {
+                        dooooo();
+                        answerSelect[j].setAttribute('eventseted', "true");
+                    });
+                }
+            }
+
+            if (answerSelect.length !== 0) {
+                answerSelectLength = answerSelect.length - 1;
+            }
+            // console.log([Number(answerSelectLength + 1), answerSelect.length]);
+            // console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        }
+
+        if (recallSelect.length > 0) {
+            if (recallSelect.length === recallSelectLength) {
+                return false;
+            }
+
+            for (let i = 0; i < recallSelect.length; i++) {
+                let j = i;
+                if (i > recallSelectLength) {
+                    const t = Number(recallSelect[i].getAttribute("data-idx")) + 1;
+                    j = t + recallSelectLength;
+                }
+                const c = recallSelect[i].getAttribute("data-idx");
+
+                const dooooo = () => {
+                    if (recallSelect[j].innerHTML === lastQuestion) {
+                        alert('[선택해서보내기] 똑같은 질의를 여러번 할 수 없습니다!');
+                        return false;
+                    }
+                    const v = `
+                        <div>
+                            ` + recallarray[i].infoSj + `
+                        </div>
+                        <div class="small">
+                            ` + recallarray[i].infoDetailCn + `
+                        </div>
+                        <button id="JSrecallSeeInfo">
+                            리콜 현황 조회하기
+                        </button>
+                    `;
+
+                    query = recallarray[i].infoSj;
+                    chatSend(recallSelect[j].innerHTML, false);
+                    botChat(v);
+
+                    const f = document.getElementById('JSrecallSeeInfo');
+                    f.addEventListener('click', () => {
+                        chatSend("상세보기" + recallSelect[j].innerHTML, false, "recallInfo", recallarray[i].infoId);
+                    });
+                    f.removeAttribute("id");
+                };
+
+                // console.log([j, c]);\
+                // console.log(answerSelect[j].getAttribute('eventseted'));
+                if (recallSelect[j].getAttribute('eventseted') !== "true") {
+                    // console.log('이벤트가 감지되지 않음');
+                    recallSelect[j].addEventListener("click", () => {
+                        dooooo();
+                        recallSelect[j].setAttribute('eventseted', "true");
+                    });
+                }
+            }
+
+            if (recallSelect.length !== 0) {
+                recallSelectLength = recallSelect.length - 1;
+            }
+            // console.log([Number(answerSelectLength + 1), answerSelect.length]);
+            // console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        }
+    };
+
     // 사용자가 메시지를 보내는 기능
-    const chatSend = (value, isajax) => {
-        console.log([lastQuestion, value, isajax]);
+    const chatSend = (value, isajax, option, query) => {
+///        console.log([lastQuestion, value, isajax]);
         if (lastQuestion === value) {
             alert('[일반보내기] 똑같은 질의를 여러번 할 수 없습니다!');
             return false;
@@ -116,6 +180,11 @@ window.addEventListener('DOMContentLoaded', () => {
             AJAX("GET", "localhost:4001/api/user/getData/" + value, null);
         }
 
+        if (option === "recallInfo") {
+            AJAX("GET", "localhost:4001/api/user/recallhistory/infoId/" + query, null);
+        }
+
+        // console.log([value, isajax, option, query]);
         chatBotBody.innerHTML += data;
         chatBotValue.value = "";
     };
@@ -235,10 +304,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
         // 요청 성공시
         xhttp.onreadystatechange = () => {
-            console.log([xhttp.readyState, xhttp.status]);
+            // console.log([xhttp.readyState, xhttp.status]);
             if (xhttp.readyState !== XMLHttpRequest.DONE) {
                 chatBotValue.setAttribute('disabled', ' disabled');
                 chatBotValue.setAttribute('placeholder', '응답을 기다리는 중이에요!');
+            } else if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 500) {
+                chatBotValue.removeAttribute('disabled', ' disabled');
+                chatBotValue.setAttribute('placeholder', '궁금하신 내용을 입력해주세요!');
+                botChat("서버 오류입니다! <br> 조금 뒤에 다시 시도해주세요!");
             } else if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
                 chatBotValue.removeAttribute('disabled', ' disabled');
                 chatBotValue.setAttribute('placeholder', '궁금하신 내용을 입력해주세요!');
@@ -246,8 +319,16 @@ window.addEventListener('DOMContentLoaded', () => {
                 // 답변 처리
                 r = xhttp.responseText;
 
+                // console.log(r);
+
                 // infoid가 있을 시, 다른 리스트 목록으로 추출
                 if (r.indexOf("infoId") !== -1) {
+                    if (r.indexOf("insert_dt") !== -1) {
+                        query =
+                            botChat(responseProcess(r, "insert_dt"));
+                        return true;
+                    }
+
                     botChat(responseProcess(r, "infoId"));
                     return true;
                 }
@@ -315,28 +396,40 @@ window.addEventListener('DOMContentLoaded', () => {
                 </button>
             `;
             }
-            console.log(r);
             return r;
         } else if (dataName === "infoId") {
-            recallList = JSON.parse(JSON.parse(d)[0]);
+            recallarray = JSON.parse(JSON.parse(d)[0]);
             // console.log(recallList);
             // 헤더
             r += `
-            지금까지 신청하신 리콜의 내역입니다.
+            고객님이 신청한 리콜은 다음과 같습니다!
             <br>
-            제목을 눌러 자세한 진행사항을 확인해 보세요.
+            제목을 눌러 리콜의 진행사항을 확인보세요.
         `;
 
             // 반복문
-            for (let i = 0; i < recallList.length; i++) {
+            for (let i = 0; i < recallarray.length; i++) {
                 r += `
                 <button class="JSrecallSelect" data-idx="` + i + `">
                 ` +
-                    recallList[i].infoSj;
+                    recallarray[i].infoSj;
                 +`
                 </button>
             `;
             }
+            return r;
+        } else if (dataName === "insert_dt") {
+            recallsubarray = JSON.parse(JSON.parse(d)[0]);
+            console.log(recallsubarray);
+            // 헤더
+            r += query + `에 대한 리콜 결과는 다음과 같습니다.
+        `;
+
+            // 반복문
+            for (let i = 0; i < recallsubarray.length; i++) {
+                r += `<div class="recallArray"> <span class="date">` +  recallsubarray[i].insert_dt + ` </span> ㅡ <span class="name">` + recallsubarray[i].user_gubun + `</span> : <span class="text"> ` + recallsubarray[i].request_remarks + ` </span> <span>(` + recallsubarray[i].status + `)</span></div>`
+            }
+
             return r;
         }
     };
@@ -358,20 +451,6 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     botChat("welcome");
-
-// 챗봇으로 텍스트를 작성하는 코드
-    chatBotValue.addEventListener('keyup', () => {
-        if (window.event.keyCode === 13) {
-            chatSend(chatBotValue.value, true);
-            bottomScroll();
-        }
-    });
-
-// 보내기 버튼이 눌렸을때 텍스트를 보내는 코드
-    chatBotSubmit.addEventListener('click', () => {
-        chatSend(chatBotValue.value, true);
-        bottomScroll();
-    });
 
 // 만약 해당 유저가 리콜을 한 전적이 있다면 recall 목록을 보여줌.
     AJAX("GET", "localhost:4001/api/user/recallhistory/userid/" + user_id, null, false);
