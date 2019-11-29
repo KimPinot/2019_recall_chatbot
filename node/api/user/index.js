44402e0546f2c93c5caa433245d09aef269faa10
KimPinot <sohuyn43@gmail.com>
2019-11-29 23:05:54 +0900

    🔨 내용의 다양성 추가

--- node/api/user/index.js
+++ node/api/user/index.js
@@ -2,6 +2,6 @@ const Router = require('koa-router');
 const user = new Router();
 const userCtrl = require('./user.ctrl');
 
-user.get('/', userCtrl.getalldata);
+user.get('/getData/:value', userCtrl.getalldata);
 
 module.exports = user;
--- node/api/user/user.ctrl.js
+++ node/api/user/user.ctrl.js
@@ -3,7 +3,7 @@ const request = require('request-promise');
 // getalldata
 exports.getalldata = async (ctx) => {
     const promise = async () => {
-        const t = encodeURI("현대자동차");
+        const t = encodeURI(ctx.params.value);
         let r = await request.get(
             {
                 uri: "https://recall.run.goorm.io/callNLPJSON?company=kisa&system=recall&classgubun=ALL&var1=" + t
